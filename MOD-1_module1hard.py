c7aa40d20e5186cb3cecd74feb2866881c852eca
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-05-21 20:41:36 +0300

    Update MOD-1_module1hard.py

--- MOD-1_module1hard.py
+++ MOD-1_module1hard.py
@@ -16,7 +16,7 @@ stud_dict[stud_list[2]] = sum(grades[2]) / len(grades[2])
 stud_dict[stud_list[3]] = sum(grades[3]) / len(grades[3])
 stud_dict[stud_list[4]] = sum(grades[4]) / len(grades[4])
 
-# print(stud_list)
+# print(stud_list)                 # проверка отсортированного списка студентов
 print(stud_dict)
 
 
